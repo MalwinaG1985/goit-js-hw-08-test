197a16a8ae3189b5b94a816df7e7b57b00ce607d
Malwina Grysiak <malwinagrysiak@gmail.com>
2023-06-17 10:11:23 +0200

    update video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import '../css/common.css';
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const onPlay = function (data) {
+    const stringifyData = JSON.stringify(data);
+    localStorage.setItem(TIME_KEY, stringityData);
+};
+
+Player.on('timeupdate', throttle(onPlay, 1000));
+
+function resumePlayback() {
+    if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+        return;
+    }
+    const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
+    if (paused) {
+        Player
+            .setCurrentTime(paused)
+            .then(function (seconds) { })
+            .catch(function (error) {
+                switch (error.name) {
+                    case 'Error': break;
+                    default: break;
+                }
+            });
+    }
+}
+resumePlayback();
